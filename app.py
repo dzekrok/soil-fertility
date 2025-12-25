985f1c4b0e3806ab1b7e7ccd2ce4d76370bbef8f
dzekrok <140779893+dzekrok@users.noreply.github.com>
2025-12-25 10:39:34 +0700

    app.py

--- app.py
+++ app.py
@@ -57,5 +57,9 @@ def index():
         recommendation = get_recommendation(label)
 
     return render_template('index.html', result=result, recommendation=recommendation)
+
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
